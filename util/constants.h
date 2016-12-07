c64a5fc7d9859706d6af267f5a18f29877b7b78d
Pinablink <pinablink@hotmail.com>
2016-12-07 16:29:52 -0200

    Adição de utilitario
    Criação de constantes para dar suporte a variados arquivos com suas rotinas.

---
+++ util/constants.h
@@ -0,0 +1,75 @@
+;----------------------------------------------------------------------------------------------
+; RANGE DE MEMORIA NECESSÁRIO PARA A UTILIZAÇÃO DA COMUNICAÇÃO
+; |----- 520 ----------------------------------------------- 552 ----- |
+;----------------------------------------------------------------------------------------------
+#define          DATA1_IN           &0x0208
+#define          DATA2_IN           &0x0209
+#define          DATA3_IN           &0x020A
+#define          DATA4_IN           &0x020B
+#define          DATA5_IN           &0x020C
+#define          DATA6_IN           &0x020D
+#define          DATA7_IN           &0x020E
+#define          DATA8_IN           &0x020F
+#define          DATA9_IN           &0x0210
+#define          DATA10_IN          &0x0211
+#define          DATA11_IN          &0x0212
+#define          DATA12_IN          &0x0213
+#define          DATA13_IN          &0x0214
+#define          DATA14_IN          &0x0215
+#define          DATA15_IN          &0x0216
+#define          DATA16_IN          &0x0217
+#define          DATA17_IN          &0x0218
+#define          DATA18_IN          &0x0219
+#define          DATA19_IN          &0x021A
+#define          DATA20_IN          &0x021B
+#define          DATA21_IN          &0x021C
+#define          DATA22_IN          &0x021D
+#define          DATA23_IN          &0x021E
+#define          DATA24_IN          &0x021F
+#define          DATA25_IN          &0x0220
+#define          DATA26_IN          &0x0221
+#define          DATA27_IN          &0x0222
+#define          DATA28_IN          &0x0223
+#define          DATA29_IN          &0x0224
+#define          DATA30_IN          &0x0225
+#define          DATA31_IN          &0x0226
+#define          DATA32_IN          &0x0227
+#define          DATA33_IN          &0x0228 
+;----------------------------------------------------------------------------------------------
+#define          NUM_1              0x001u
+#define          NUM_2              0x002u
+#define          NUM_3              0x003u
+#define          NUM_4              0x004u
+#define          NUM_5              0x005u
+#define          NUM_6              0x006u
+#define          NUM_7              0x007u
+#define          NUM_8              0x008u
+#define          NUM_9              0x009u
+#define          NUM_10             0x00Au    
+#define          NUM_11             0x00Bu
+#define          NUM_12             0x00Cu 
+#define          NUM_13             0x00Du
+#define          NUM_14             0x00Eu 
+#define          NUM_15             0x00Fu
+#define          NUM_16             0x010u  
+#define          NUM_17             0x011u 
+#define          NUM_18             0x012u
+#define          NUM_19             0x013u
+#define          NUM_20             0x014u
+#define          NUM_21             0x015u 
+#define          NUM_22             0x016u
+#define          NUM_23             0x017u 
+#define          NUM_24             0x018u
+#define          NUM_25             0x019u
+#define          NUM_26             0x01Au
+#define          NUM_27             0x01Bu
+#define          NUM_28             0x01Cu
+#define          NUM_29             0x01Du
+#define          NUM_30             0x01Eu
+#define          NUM_31             0x01Fu
+#define          NUM_32             0x020u
+#define          NUM_33             0x021u
+;----------------------------------------------------------------------------------------------
+#define          DATA_BUFFER        R5
+#define          COUNT              R6
+;----------------------------------------------------------------------------------------------
